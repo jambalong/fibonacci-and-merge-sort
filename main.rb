398549f6c598aa3fdb0e54e70074415660e34ad2
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-06-14 23:51:46 -0700

    Add fibs_rec()

--- main.rb
+++ main.rb
@@ -5,3 +5,12 @@ def fibs(n)
 
   n == 0 ? [0] : fib_arr
 end
+
+def fibs_rec(n)
+  return [0] if n == 0
+  return [0, 1] if n == 1
+
+  fib_arr = fibs_rec(n - 1)
+
+  fib_arr << fib_arr[-1] + fib_arr[-2]
+end
